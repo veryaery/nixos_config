895423c50f24fcb94e2c2657aa1f251f22447bad
aery <aeryowo@gmail.com>
2022-06-12 13:37:01 +0200

    alacritty, layouts

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -1,11 +1,27 @@
 import XMonad
 
-config :: XConfig
-config = def
+import XMonad.Layout
+import XMonad.Layout.SimpleFloat
+ 
+layout' = tall ||| Full ||| simpleFloat
+    where
+        nmaster = 1
+        ratio = 1/2
+        delta = 3/100
+
+        tall = Tall nmaster delta ratio
+
+config' :: XConfig
+config' = def
     {
         terminal = "alacritty",
-        modMask = mod4Mask
+        modMask = mod4Mask,
+        layoutHook = layout',
+
+        borderWidth = 2,
+        normalBorderColor = "#404040",
+        focusedBorderColor = "#ffffff"
     }
 
 main :: IO ()
-main = xmonad config
+main = xmonad config'
