57b8e3c7023aefa49d912570329119b74e97c54b
aery <aeryowo@gmail.com>
2022-06-15 01:50:04 +0200

    restart

--- modules/os/rice/os.nix
+++ modules/os/rice/os.nix
@@ -7,6 +7,7 @@
         displayManager.gdm.enable = true;
         windowManager.xmonad = {
             enable = true;
+            xmonadCliArgs = [ "--restart" ];
             
             extraPackages = haskellPackages: with haskellPackages; [
                 xmonad-contrib
