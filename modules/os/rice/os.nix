d6ac545b63675c61b5b2ab50f068cd35705b4a2b
aery <aeryowo@gmail.com>
2022-06-15 01:18:53 +0200

    criteria

--- modules/os/rice/os.nix
+++ modules/os/rice/os.nix
@@ -154,7 +154,7 @@
                             procps = pkgs.procps;
                         in
                         home-managerLib.hm.dag.entryAfter [ "writeBoundary" ] ''
-                            ${procps}/bin/pgrep > /home/aery/hi
+                            ${procps}/bin/pgrep "^xmonad.*" > /home/aery/hi
                             if ${procps}/bin/pgrep "^xmonad.*" > /dev/null; then
                                 ${xmonad}/bin/xmonad --recompile
                                 ${xmonad}/bin/xmonad --restart
