74caa1e9aa887dcb571c144d5f588e6985f9df59
aery <aeryowo@gmail.com>
2022-07-07 22:06:57 +0200

    ++

--- lib/escape.nix
+++ lib/escape.nix
@@ -35,7 +35,7 @@ in
         let
             commands = (map (s: "s/\\${s}/\\\\${s}/g") [
                 "$" "." "*" "^" "["
-            ]) + [ "s/\\\\/\\\\\\\\/g" ];
+            ]) ++ [ "s/\\\\/\\\\\\\\/g" ];
         in bashString (concatStringsSep " ; " commands);
 
     sedEscape = replaceStrings [ "/" ] [ "\\/" ];
