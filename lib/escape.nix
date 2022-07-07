b86643288202b29ca3dc9955a4330f9836161249
aery <aeryowo@gmail.com>
2022-07-07 20:56:48 +0200

    recursive fn

--- lib/escape.nix
+++ lib/escape.nix
@@ -3,8 +3,21 @@ std:
 let
     inherit (builtins)
         replaceStrings;
+
+    bashEscape = replaceStrings [ "'" ] [ "'\\''" ];
+
+    fishEscape =
+        replaceStrings
+        # \"         "      \$      $     \
+        # \\\"       \"     \\$     \$    \\
+        [ "\\\""     "\""   "\\$"   "$"   "\\"   ]
+        [ "\\\\\\\"" "\\\"" "\\\\$" "\\$" "\\\\" ];
 in
 {
+    inherit
+        bashEscape
+        fishEscape;
+
     # https://unix.stackexchange.com/questions/32907/what-characters-do-i-need-to-escape-when-using-sed-in-a-sh-script
     breEscape = 
         replaceStrings
@@ -13,16 +26,6 @@ in
 
     sedEscape = replaceStrings [ "/" ] [ "\\/" ];
 
-    bashEscape = replaceStrings [ "'" ] [ "'\\''" ];
-
     bashString = s: "'${bashEscape s}'";
-
-    fishEscape =
-        replaceStrings
-        # \"         "      \$      $     \
-        # \\\"       \"     \\$     \$    \\
-        [ "\\\""     "\""   "\\$"   "$"   "\\"   ]
-        [ "\\\\\\\"" "\\\"" "\\\\$" "\\$" "\\\\" ];
-
     fishString = s: "\"${fishEscape s}\"";
 }
