d04ea0334f2ff4c6b293930b6fbb93e31e4a5eef
aery <aeryowo@gmail.com>
2022-07-11 12:30:15 +0200

    installtheme

--- derivations/installtheme.nix
+++ derivations/installtheme.nix
@@ -22,52 +22,40 @@ pkgs.writeScriptBin
         return 1
     end
     set -l dotfiles $(cat $file)
-    # set -l escapeDotfiles $(echo $dotfiles | sed ${escapeBREScriptFish} | sed ${escapeSEDScriptFish})
+    
+    set -l themelog ~/.themelog
 
-    # function remove_file_and_parents
-    #     set -l file $argv[1]
+    if [ -e $themelog ]
+        set -l newlog
 
-    #     rm $file
-
-    #     set -l segments $(string split / $(dirname $file))
-    #     set -l i $(count $segments)
-    #     while [ $i -gt 0 ]
-    #         set -l path $(string join / $segments[$(seq $i)])
-    #         if [ -z "$(ls -A $path)" ]
-    #             rm -r $path
-    #             echo Removed directory $path
-    #         end
-
-    #         set i $(math $i - 1)
-    #     end
-    # end
-
-    set -l prevtheme ~/.prevtheme
-
-    if [ -e $prevtheme ]
-        set -l files $(cat $prevtheme)
+        set -l files $(cat $themelog)
         for file in $files
             set -l dotfile $dotfiles/$file
             set -l homefile ~/$file
 
             if [ -e $dotfile ]
+                # file is still in dotfiles.
+                set -a newlog $file # keep the log entry.
                 continue
             end
-            # file is no longer in dotfiles
+            # file is no longer in dotfiles.
 
             if [ -f $homefile ]
-                # file is a regular file
+                # file is a regular file.
                 rm $homefile
                 echo Removed regular file $file
             end
             if [ -d $homefile ] && [ -z "$(ls -A $homefile)" ]
-                # file is an empty directory
+                # file is an empty directory.
                 rm $homefile
                 echo Removed empty directory $file
             end
         end
 
-        truncate -s 0 $prevtheme
+        truncate -s 0 $themelog
+        for entry in $newlog
+            echo $entry >> $themelog
+        end
     end
 
     set -l files $(find $dotfiles -type f)
@@ -75,26 +63,26 @@ pkgs.writeScriptBin
         set -l file $(string split -n -m1 $dotfiles/ $dotfile)
         set -l homefile ~/$file
 
+        set -l dirlog
+        
         set -l segments $(string split / $(dirname $file))
         set -l segments_count $(count $segments)
         set -l i 1
-        set -l created_dirs
-        
         while [ $i -le $segments_count ]
             set -l dir $(string join / $segments[$(seq $i)])
             set -l homedir ~/$dir
 
             if [ ! -e $homedir ]
                 mkdir $homedir
-                set -p created_dirs $dir
+                set -p dirlog $dir
                 echo Created directory $dir
             end
 
             set i $(math $i + 1)
         end
 
-        for dir in $created_dirs
-            echo $dir >> $prevtheme
+        for entry in $dirlog
+            echo $entry >> $themelog
         end
 
         if [ -e $homefile ]
@@ -103,10 +91,10 @@ pkgs.writeScriptBin
             echo Updated regular file $file
         else
             cat $dotfile >> $homefile
+            echo $file >> $themelog
             echo Created regular file $file
         end
 
-        echo $file >> $prevtheme
     end
 
     echo $theme: Theme installed.
