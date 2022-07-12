76f8ae3e91d7236ac7e359ec4ec6aba26d6c0417
aery <aeryowo@gmail.com>
2022-07-12 13:35:44 +0000

    removed trailing comma

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -34,7 +34,7 @@ treesitter.setup {
 
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = false
     },
 
     indent = {
