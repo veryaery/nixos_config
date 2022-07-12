0578a6a64c25a68a3f643a67513998f468169220
aery <aeryowo@gmail.com>
2022-07-12 13:09:00 +0200

    nix settings

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,6 +1,10 @@
 vim.cmd([[ set packpath+=<packpath> ]])
 
 vim.opt.number = true
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = -1
 vim.opt.shiftwidth = 0
+
+vim.g.nix_recommended_style = false
