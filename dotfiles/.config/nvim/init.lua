9b6127d5a442a44696b509fcc1bde607f74a06da
aery <aeryowo@gmail.com>
2022-08-05 19:59:35 +0200

    bound jj to esc

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -11,6 +11,8 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.splitright = true
 
+vim.keymap.set("i", "jj", "<Esc>")
+
 vim.keymap.set("n", "<A-l>", "<C-w>l")
 vim.keymap.set("n", "<A-h>", "<C-w>h")
 vim.keymap.set("n", "<A-j>", "<C-w>j")
